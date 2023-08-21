1b5d5c4c2802339546784fed1d6bc06c080e299f
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-21 10:44:30 -0300

    [feat] pretty print for AST

---
+++ src/ast/pprint_ast.ml
@@ -0,0 +1,20 @@
+open Ast_types
+open Core
+
+let indent_space = "   "
+
+let pprint_modifier ppf ~indent modifier =
+  Fmt.pf ppf "%sModifier: %s@." indent (string_of_modifier modifier)
+
+let pprint_type_expr ppf ~indent type_expr =
+  Fmt.pf ppf "%sType expr: %s@." indent (string_of_type type_expr)
+
+let pprint_param ppf ~indent = function
+| TParam (type_expr, param_name) ->
+  Fmt.pf ppf "%sParam: %s@." indent (Var_name.to_string param_name);
+  let new_indent = indent_space ^ indent in
+  pprint_type_expr ppf ~indent:new_indent type_expr
+
+let pprint_params ppf ~indent = function
+  | []     -> Fmt.pf ppf "%sParam: %s@." indent (string_of_type TEVoid)
+  | params -> List.iter ~f:(pprint_param ppf ~indent) params
---
+++ src/ast/pprint_ast.mli
@@ -0,0 +1,12 @@
+(** This module defines a set of pretty printing functions for the parts of the AST that are common across the parsed and typed ASTs - to be used by pretty printing functions
+of the two ASTs.
+They all take in a formatter which specifies output channel e.g. a string formatter or
+a stdout formatter. (Think of this as a generalisation of printf)
+The second argument for all of these is an indent - this corresponds to nesting depth
+within the AST *)
+
+open Ast_types
+
+val pprint_modifier : Format.formatter -> indent:string -> modifier -> unit
+val pprint_type_expr : Format.formatter -> indent:string -> type_expr -> unit
+val pprint_params : Format.formatter -> indent:string -> param list -> unit
