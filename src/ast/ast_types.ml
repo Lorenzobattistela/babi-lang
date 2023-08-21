d0be47e4311e314692488254431b45ba1662b7ef
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-21 10:44:11 -0300

    [feat] configuring ast types

---
+++ src/ast/ast_types.ml
@@ -0,0 +1,84 @@
+open Base
+
+type loc = Lexing.position
+
+let string_of_loc loc =
+  Fmt.str "Line:%d Position:%d" loc.Lexing.pos_lnum
+    (loc.Lexing.pos_cnum - loc.Lexing.pos_bol + 1)
+
+module type ID = sig
+  type t
+
+  val of_string : string -> t
+  val to_string : t -> string
+  val ( = ) : t -> t -> bool
+end
+
+module String_id = struct
+  type t = string
+
+  let of_string x = x
+  let to_string x = x
+  let ( = ) = String.( = )
+end
+
+module Var_name : ID = String_id
+module Method_name : ID = String_id
+module Function_name : ID = String_id
+
+type modifier = MRec
+
+let string_of_modifier = function MRec -> "Rec"
+
+type type_expr =
+  | TEInt
+  | TEVoid
+  | TEBool
+
+let string_of_type = function
+  | TEInt -> "Int"
+  | TEVoid -> "Void"
+  | TEBool -> "Bool"
+
+
+type param =
+  | TParam of type_expr * Var_name.t
+
+let get_params_types params =
+  List.map ~f:(fun (TParam (param_type, _)) -> param_type) params
+
+
+(* BINARY OPERATORS *)
+type bin_op =
+  | BinOpPlus
+  | BinOpMinus
+  | BinOpMult
+  | BinOpIntDiv
+  | BinOpRem
+  | BinOpLessThan
+  | BinOpLessThanEq
+  | BinOpGreaterThan
+  | BinOpGreaterThanEq
+  | BinOpAnd
+  | BinOpOr
+  | BinOpEq
+  | BinOpNotEq
+
+let string_of_bin_op = function
+  | BinOpPlus          -> "+"
+  | BinOpMinus         -> "-"
+  | BinOpMult          -> "*"
+  | BinOpIntDiv        -> "/"
+  | BinOpRem           -> "%"
+  | BinOpLessThan      -> "<"
+  | BinOpLessThanEq    -> "<="
+  | BinOpGreaterThan   -> ">"
+  | BinOpGreaterThanEq -> ">="
+  | BinOpAnd           -> "&&"
+  | BinOpOr            -> "||"
+  | BinOpEq            -> "=="
+  | BinOpNotEq         -> "!="
+
+type un_op = UnOpNot | UnOpNeg
+
+let string_of_un_op = function UnOpNot -> "!" | UnOpNeg -> "-"
---
+++ src/ast/ast_types.mli
@@ -0,0 +1,63 @@
+(** These are the types used in constructing any AST for a Bolt program. Bolt programs
+    consist of a list of class definitions, followed by a list of function definitions and
+    finally an expression to execute. *)
+
+    type loc = Lexing.position
+    (** Stores the line and position of the token *)
+    
+    (** An abstract type for identifiers*)
+    module type ID = sig
+      type t
+    
+      val of_string : string -> t
+      val to_string : t -> string
+      val ( = ) : t -> t -> bool
+    end
+    
+    (** We distinguish the different identifiers for variables, classes, traits and fields. *)
+    
+    module Var_name : ID
+    module Method_name : ID
+    module Function_name : ID
+    
+    type modifier = MRec (** function is recursive *)    
+
+    (** Define types of expressions in Bolt programs*)
+    type type_expr =
+      | TEInt
+      | TEVoid
+      | TEBool
+            
+    (** Parameter of a function *)
+    type param =
+      | TParam of type_expr * Var_name.t
+    
+    val get_params_types : param list -> type_expr list
+    
+    (** Binary operators for expressions *)
+    
+    type bin_op =
+      | BinOpPlus
+      | BinOpMinus
+      | BinOpMult
+      | BinOpIntDiv
+      | BinOpRem
+      | BinOpLessThan
+      | BinOpLessThanEq
+      | BinOpGreaterThan
+      | BinOpGreaterThanEq
+      | BinOpAnd
+      | BinOpOr
+      | BinOpEq
+      | BinOpNotEq
+    
+    type un_op = UnOpNot | UnOpNeg
+    
+    (** Various helper functions to convert types to equivalent string representations *)
+    
+    val string_of_loc : loc -> string
+    val string_of_modifier : modifier -> string
+    val string_of_type : type_expr -> string
+    val string_of_bin_op : bin_op -> string
+    val string_of_un_op : un_op -> string
+    
