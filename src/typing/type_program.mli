276edd667bc9fe0c3302a3ea4be35dae22b298b2
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-24 14:48:49 -0300

    [feat] type checking program

---
+++ src/typing/type_program.mli
@@ -0,0 +1,9 @@
+(** Given the parsed AST of a bolt program, type-check it and if type-checking succeeds
+    return the AST with type annotations.*)
+
+open Core
+
+val type_program : Parsing.Parsed_ast.program -> Typed_ast.program Or_error.t
+
+val pprint_typed_ast : Format.formatter -> Typed_ast.program -> unit
+(** Given a formatter and typed AST, pretty-print the AST - useful for debugging *)
