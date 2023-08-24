24cda876935b3259b04ae7981182b8dbf844863d
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-24 14:38:58 -0300

    [feat] typr-checking expressions

---
+++ src/typing/type_expr.mli
@@ -0,0 +1,17 @@
+(** Infer type parsed expressions given the function definitions - return the
+    expression annotated with types if type-checking succeeds. *)
+open Ast.Ast_types
+open Core
+open Type_env
+
+val type_expr :
+    Parsing.Parsed_ast.function_defn list
+    -> Parsing.Parsed_ast.expr
+    -> type_env
+    -> (Typed_ast.expr * type_expr) Or_error.t
+
+val type_block_expr :
+    Parsing.Parsed_ast.function_defn list
+    -> Parsing.Parsed_ast.block_expr
+    -> type_env
+    -> (Typed_ast.block_expr * type_expr) Or_error.t
