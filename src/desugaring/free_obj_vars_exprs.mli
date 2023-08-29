ab50cac446db8a97d212e272f392f0261da09aa8
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-29 17:00:12 -0300

    [fix] trying to fix

--- src/desugaring/free_obj_vars_exprs.mli
+++ src/desugaring/free_obj_vars_exprs.mli
@@ -1,6 +1,6 @@
 open Ast.Ast_types
 
-val free_obj_vars_expr : Typing.Typed_ast.expr -> (Var_name.t * Class_name.t) list
+val free_obj_vars_expr : Typing.Typed_ast.expr -> (Var_name.t) list
 
 val free_obj_vars_block_expr :
-  Typing.Typed_ast.block_expr -> (Var_name.t * Class_name.t) list
+  Typing.Typed_ast.block_expr -> (Var_name.t) list
