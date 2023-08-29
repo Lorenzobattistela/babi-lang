b77666774b749d80ade9768662d54e6b6f9ededd
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-29 16:59:17 -0300

    [fix] compile program IR file

--- src/compile_program_ir.mli
+++ src/compile_program_ir.mli
@@ -1,7 +1,10 @@
-open Core
+(** This is the entry point for execution of Bolt programs. *)
 
-val type_function_defns :
-  Parsing.Parsed_ast.function_defn list
-  -> Typed_ast.function_defn list Or_error.t
-(** Type-check function defns, using types defined in the class defns, and return the
-    typed function defn augmented with type information *)
+val compile_program_ir :
+     ?should_pprint_past:bool (** whether to print parsed AST *)
+  -> ?should_pprint_tast:bool (** whether to print typed AST *)
+  -> ?should_pprint_dast:bool (** whether to print desugared AST *)
+  -> ?should_pprint_fir:bool (** whether to print serialisable frontend IR *)
+  -> ?compile_out_file:string (** output serialised protobuf to a file *)
+  -> Lexing.lexbuf (* Lex buffer to read the program from *)
+  -> unit
