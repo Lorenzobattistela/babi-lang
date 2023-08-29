b1dd6307676504362a69e403f8e85a1c3b35975a
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-29 16:59:53 -0300

    [fix] desugar program errors

--- src/desugaring/desugar_program.ml
+++ src/desugaring/desugar_program.ml
@@ -4,16 +4,14 @@ open Desugar_function_defns
 open Remove_variable_shadowing
 open Pprint_dast
 
-let desugar_program (Typing.Typed_ast.Prog (class_defns, function_defns, main_expr)) =
-  List.map ~f:(desugar_class_defn class_defns) class_defns
-  |> fun desugared_class_defns ->
-  List.map ~f:(desugar_function_defn class_defns) function_defns
+let desugar_program (Typing.Typed_ast.Prog ( function_defns, main_expr)) =
+  List.map ~f:(desugar_function_defn) function_defns
   |> fun desugared_function_defns ->
-  desugar_block_expr class_defns [] main_expr
+  desugar_block_expr main_expr
   |> fun desugared_main_expr ->
   let desugared_program =
     Desugared_ast.Prog
-      (desugared_class_defns, desugared_function_defns, desugared_main_expr) in
+      (desugared_function_defns, desugared_main_expr) in
   remove_var_shadowing_program desugared_program
 
 let pprint_desugared_ast ppf prog = pprint_program ppf prog
